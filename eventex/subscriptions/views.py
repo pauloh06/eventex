53eae886e18287147fd2442545a83468e3f00b53
PH <ph@MacBook-Pro-de-PH.local>
2017-06-06 01:37:39 -0300

    Corrige a subscription view

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -38,5 +38,5 @@ def new(request):
 
 
 def _sendmail(subject, from_, to, template_name, context):
-    body = render_to_string('subscriptions/subscription_email.txt', context)
+    body = render_to_string(template_name, context)
     mail.send_mail(subject, body, from_, [from_, to])
