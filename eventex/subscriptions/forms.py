c606d92a8c8426245a25d1af10ace9da106cfb61
PH <ph@MacBook-Pro-de-PH.local>
2017-06-11 22:32:27 -0300

    Email or Phone are optionals

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -13,14 +13,20 @@ def validate_cpf(value):
 class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF', validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
     def clean_name(self):
-        # ex: PAULO henrique
-        name = self.cleaned_data['name']
+        # Capitalize name
+        name = self.cleaned_data.get('name')
         words = []
 
         # this is a list comprehension
         words = [w.capitalize() for w in name.split()]
         return ' '.join(words)
+
+    def clean(self):
+        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+
+        return self.cleaned_data
--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -77,6 +77,14 @@ class SubscriptionsNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
 
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Paulo Henrique', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
+
+
 # @unittest.skip('To be removed')
 # class SubscribeSuccessMessage(TestCase):
 #     def test_message(self):
