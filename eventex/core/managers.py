11049592b05a945156a437ff71ec1b60bcc85020
PH <ph@MacBook-Pro-de-PH.local>
2017-07-11 16:57:51 -0300

    Mixins

--- eventex/core/managers.py
+++ eventex/core/managers.py
@@ -8,7 +8,7 @@ class KindQuerySet(models.QuerySet):
     def phones(self):
         return self.filter(kind=self.model.PHONE)
 
-class PeriodManager(models.Manager):
+class PeriodQuerySet(models.QuerySet):
     MIDDAY = '12:00'
 
     def at_morning(self):
@@ -16,3 +16,6 @@ class PeriodManager(models.Manager):
 
     def at_afternoon(self):
         return self.filter(start__gte=self.MIDDAY)
+
+
+PeriodManager = models.Manager.from_queryset(PeriodQuerySet)
